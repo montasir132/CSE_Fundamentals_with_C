8552dc53ebb38bfcd97f4a37b04c0e507cf520cb
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-15 00:34:47 +0600

    Delete Module_01 directory

--- Module_01/Data_type_limitations_in.C
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    //code
-    long long int a = 100000000000000;
-    printf("a: %lld\n", a);
-    double f = 34549565.52254564423;
-    printf("f: %lf\n", f);
-    return 0;//Exit
-}
--- Module_01/First_C_Program.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    //code
-    printf("Hello world\n");
-    printf("Hello\tBangladesh");
-    return 0;//Exit
-}
