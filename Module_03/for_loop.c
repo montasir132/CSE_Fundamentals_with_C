9af160769e7ebd7ce79d45946849ce64b2b0e329
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-15 00:35:07 +0600

    Delete Module_03 directory

--- Module_03/for_loop.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for(int i=1; i<=100000 ; i+=1)
-    {
-        printf("%d\n",i);
-    }
-    return 0;
-}
--- Module_03/infinity_loop.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    // for(int i=1; i>=1 ; i+=1) //denger
-    {
-        printf("%d\n",i);
-    }
-    return 0;
-}
