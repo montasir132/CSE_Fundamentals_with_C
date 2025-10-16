6813eeb0d540e74f77c0d4265fcc47bd531aa621
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-17 00:11:14 +0600

    Add files via upload

---
+++ practice_problem/week1/problem1&2.c
@@ -0,0 +1,131 @@
+// ------module 1 ex porblem solving------
+
+// Write a C program that will take 2 numbers from the user and then print the 2nd number first and then first number.  
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d %d", b, a);
+//     return 0;
+// }
+
+
+
+
+// ------module 2 ex porblem solving------
+
+// Take a number from user and check if its a even number or odd number.
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     if(n % 2 == 0)
+//     {
+//         printf("Even");
+//     }
+//     else{
+//         printf("Odd");
+//     }
+//     return 0;
+// }
+
+// Take a number from user and check if its a positive or negative number.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     if(n >= 0)
+//     {
+//         printf("Positive");
+//     }
+//     else{
+//         printf("Negative");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// -----Practice Day 01 -----
+
+// Zero or Non Zero url:https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82wqkjv002yr001ug30yttk
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     if(n == 0)
+//     {
+//         printf("Zero");
+//     }
+//     else{
+//         printf("Non Zero");
+//     }
+//     return 0;
+// }
+
+
+
+// Multiple or not url:https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82x5rnr003er00114f9qwvx
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     if(a % b == 0 || b % a == 0)
+//     {
+//         printf("Yes");
+//     }
+//     else{
+//         printf("No");
+//     }
+//     return 0;
+// }
+
+
+// 5 Marks for good hand writing url:https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82wxa4y0036r001pze0gx3l
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+//     printf("%d", n + 5);
+//     return 0;
+// }
+
+
+
+// Floating Point Number url:https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82xeb86003kr0017tvabqfc
+
+// #include<stdio.h>
+// int main(){
+//     float x;
+//     scanf("%f", &x);
+//     printf("%.3f", x);
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    fgets(s, sizeof(s), stdin);
+    printf("Hello, World! \n");
+    printf("%s", s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
