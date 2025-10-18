587c7c9685c7d58952c47d44b710f61b563c01a7
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-19 00:39:08 +0600

    Add files via upload

---
+++ Module_09 [ Array Operations ]/Insert.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n+1];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int ind,valu;
+    scanf("%d %d",&ind,&valu);
+    for(int i=n; i >= ind+1; i--)
+    {
+        a[i]=a[i-1];
+    }
+    a[ind]=valu;
+    for(int i=0;i<=n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
